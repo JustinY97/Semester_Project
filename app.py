360fb204e09f50aed0b2ef7cc07cca735315543a
Justin <69814530+JustinY97@users.noreply.github.com>
2021-12-08 20:30:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -151,8 +151,8 @@ def logout():
 @app.route('/create_account')
 def createUser():
     form = create_user_form()
-    date = datetime.strptime(str(form.birthYear.data + '-' + form.birthMonth.data + '-' + form.birthDay), '%Y-%m-%d')
-    sql = str('EXECUTE NewUser(%s, %s, %s, %s, %s, %d, %d, %s, %s, %d)' % (form.name.data, form.password.data, date, form.gender.data, form.weight.data, form.height.data, form.activity.data, form.diet.data, form.budget.data))
+    date = datetime(form.birthYear.data, form.birthMonth.data, form.birthDay.data)
+    sql = str('EXECUTE NewUser(%s, %s, %s, %s, %s, %d, %d, %s, %s, %d)' % (form.name.data, form.password.data, str(date), form.gender.data, form.weight.data, form.height.data, form.activity.data, form.diet.data, form.budget.data))
     
     connection = pymysql.connect(host='cmsc508projectdb.colnzg9d22sk.us-east-2.rds.amazonaws.com',user='master', password='CMSC508Project', database='CMSC508Project', cursorclass=pymysql.cursors.DictCursor)
 
