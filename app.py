b96aec969d10884a3f66328ced2e34dbc116453e
Justin <69814530+JustinY97@users.noreply.github.com>
2021-12-08 21:41:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def createUser():
         month = form.birthMonth.data
         day = form.birthDay.data
         date = dt.datetime(year, month, day)
-        sql = str('CALL NewUser(%s, %s, %s, %s, %d, %d, %s, %s, %d)' % (form.name.data, form.password.data, date.strftime("%B"), form.gender.data, form.weight.data, form.height.data, form.activity.data, form.diet.data, form.budget.data))
+        sql = str('CALL NewUser(%s, %s, %s, %s, %d, %d, %s, %s, %d)' % (form.name.data, form.password.data, date.strftime("%B"), form.gender.data, form.weight.data, form.height.data, str(form.activity.data), form.diet.data, form.budget.data))
         
         with connection:
                 with connection.cursor() as cursor:
